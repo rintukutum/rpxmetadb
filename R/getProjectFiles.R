3e8369dde21825ec3833fcff84604add9ebd87a4
rintukutum <rintukutum@gmail.com>
2017-10-04 15:50:39 +0530

    modified getProjectFiles

--- R/getProjectFiles.R
+++ R/getProjectFiles.R
@@ -4,45 +4,24 @@
 #' pride ID
 #' @export
 getProjectFiles <- function(prideID){
-  data(prideDF)
+  data("prideDF")
   # prideID = 'PXD000044'
-  idx <- grep(prideID,prideDF$prideID)
+  idx <- grep(paste0('^',prideID,'$'),
+              prideDF$prideID)
   ftplink <- prideDF$ftp[idx]
   dat.ftp <-list()
   for( i in 1:length(ftplink)){
     tmpfile <- prideID
     cmd <- paste0("curl -s ",
                   ftplink[i],
-                  "README.txt > ",
+                  " > ",
                   tmpfile
     )
 
     system(cmd)
-    dat <- readLines(tmpfile)
-
-    if(length(dat) != 0){
-      dat <- read.table(tmpfile,
-                        sep='\t',
-                        header = TRUE,
-                        stringsAsFactors = FALSE)
-      dat$url <- gsub(' ','%20',dat$URI)
-      dat.ftp[[i]] <- data.frame(prideID = prideID,
-                                 dat)
-
-    }else{
-      dat.ftp[[i]] <- NA
-    }
-    unlink(tmpfile)
+    dat.ftp[[i]] <- readLines(tmpfile)
   }
-  check.content <- sapply(dat.ftp,nrow)
-  idx.null <- which(sapply(check.content,is.null))
-  if(length(idx.null) != 0){
-    #----
-    # some repos with two submission
-    dat <- dat.ftp[-idx.null][[1]]
-  }else{
-    # include both if necessary
-    dat <- plyr::ldply(dat.ftp)
-  }
+  unlink(tmpfile)
+  dat <- dat.ftp
   return(dat)
 }
